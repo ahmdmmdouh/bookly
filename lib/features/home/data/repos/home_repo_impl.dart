5709366d5c2668449471f3dcc3bbc44b59685b62
Ahmed Mamdouh <ah.mmdouh96@gmail.com>
2024-02-04 14:00:50 +0200

    create Api Service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeReposImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
